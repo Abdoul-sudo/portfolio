58714b0674d0bf7a5ccf4aa69eb3a0753a1c1e6b
Abdoul-sudo <ismaelabdoul7@gmail.com>
2023-06-29 22:24:21 +0300

    test prod

--- src/assets/index.js
+++ src/assets/index.js
@@ -44,7 +44,7 @@ import tripguide from './projects/tripguide.png';
 import onirix from './projects/onirix.png';
 
 import profile from './abdoul.jpg';
-import cv from '../../public/cv.pdf';
+import cv from '../../public/ISMAEL Abdoul Wahhaab CV.pdf';
 
 export {
   logo,
