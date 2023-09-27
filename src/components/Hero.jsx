6e0c33d179663ccfbe35c066a16646d758af6b98
Abdoul-sudo <ismaelabdoul7@gmail.com>
2023-09-27 18:17:54 +0300

    update cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,7 +58,7 @@ const Hero = () => {
               <div className="mt-7 flex flex-nowrap">
                 <a
                   href={cv}
-                  download="ISMAEL Abdoul Wahhaab CV"
+                  download="ISMAEL Abdoul Wahhaab CV.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className={`interactable text-[#dfd9ff] font-medium 2xl:text-[22px] xl:text-[20px] text-[14px] rounded-full border-2 border-white px-[28px] py-[12px] shadow-[0_0_7px_#fff] hover:shadow-none whitespace-nowrap  `}
