4b3c744319c8804151158ef5665e760653e3c60d
Abdoul-sudo <ismaelabdoul7@gmail.com>
2023-12-01 11:48:34 +0300

    fix: trailer hidden on mobile

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,12 +1,17 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  console.log('🚀 ~ file: Ball.jsx:9 ~ Ball ~ props.imgUrl:', props.imgUrl);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
@@ -14,7 +19,12 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
@@ -29,7 +39,11 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameloop="demand" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
--- src/components/cursor/Trailer.jsx
+++ src/components/cursor/Trailer.jsx
@@ -5,28 +5,24 @@ import { faLink, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Trailer = () => {
   const [icon, setIcon] = useState(faStar);
-
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  console.log('ScreenSize: ', screenSize);
   useEffect(() => {
     const trailer = document.getElementById('trailer');
 
     const getTrailerClass = (type) => {
-      if (type == 'link') {
+      if (type === 'link') {
         setIcon(faLink);
       } else {
         setIcon(faStar);
       }
     };
 
-    /**
-     * interacting2: card and navbarLink
-     * interacting:  button
-     */
     const animateTrailer = (e, interacting, interacting2) => {
       const x = e.clientX - trailer.offsetWidth / 2;
       const y = e.clientY - trailer.offsetHeight / 2;
 
       const keyframes = {
-        // zIndex: interacting ? -1 : 2,
         transform: `translate(${x}px, ${y}px) scale(${
           interacting ? 2.5 : interacting2 ? 1.5 : 1
         })`,
@@ -55,24 +51,29 @@ const Trailer = () => {
         interacting2 = interactable2 !== null;
 
       animateTrailer(e, interacting, interacting2);
+    };
 
-      // trailer.dataset.type = interacting ? interactable.dataset.type : '';
-
-      // if (interacting) {
-      //   icon.className = getTrailerClass(interactable.dataset.type);
-      // }
+    const handleResize = () => {
+      setScreenSize((prevSize) => {
+        const newSize = window.innerWidth;
+        if (prevSize !== newSize) {
+          return newSize;
+        }
+        return prevSize;
+      });
     };
 
     window.addEventListener('mousemove', detectInteractable);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      // Nettoyez l'écouteur d'événements lorsque le composant est démonté.
       window.removeEventListener('mousemove', detectInteractable);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <div id="trailer">
+    <div id="trailer" className={`${screenSize <= 1200 ? '!hidden' : ''}`}>
       {/* <FontAwesomeIcon icon={icon} id="trailer-icon" /> */}
     </div>
   );
