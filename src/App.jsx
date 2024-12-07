ae806cea152429746232d2803def0831d05a8d8d
aismael <aismael@nextaura.com>
2024-12-07 17:37:40 +0300

    fix responsive scroll x bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,39 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Footer,
-  Tech,
-  Works,
-  StarsCanvas,
-  Skills,
-  Trailer,
-  ScaleImage,
-} from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Footer, Tech, Works, StarsCanvas, Skills, Trailer, ScaleImage } from './components';
 import './App.scss';
 import { useState } from 'react';
 
 const App = () => {
-  const [imgScale, setImgScale] = useState('');
-  const setImage = (img) => {
-    setImgScale(img);
-  };
+    const [imgScale, setImgScale] = useState('');
+    const setImage = (img) => {
+        setImgScale(img);
+    };
 
-  return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        {imgScale && <ScaleImage src_data={imgScale} setImage={setImage} />}
-        <Trailer />
-        <StarsCanvas />
-        {/* <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> */}
-        <Navbar />
-        <Hero />
-        {/* </div> */}
-        <About />
-        {/* <Experience /> */}
-        {/* <Tech /> */}
-        <Skills />
+    return (
+        <BrowserRouter>
+            <div className='relative z-0 bg-primary overflow-x-hidden'>
+                {imgScale && <ScaleImage src_data={imgScale} setImage={setImage} />}
+                <Trailer />
+                <StarsCanvas />
+                {/* <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> */}
+                <Navbar />
+                <Hero />
+                {/* </div> */}
+                <About />
+                {/* <Experience /> */}
+                {/* <Tech /> */}
+                <Skills />
 
-        <Works setImage={setImage} />
-        {/* <Feedbacks /> */}
-        <div className="relative z-0 pb-24 lg:pb-32">
-          <Contact />
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+                <Works setImage={setImage} />
+                {/* <Feedbacks /> */}
+                <div className='relative z-0 pb-24 lg:pb-32'>
+                    <Contact />
+                </div>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
